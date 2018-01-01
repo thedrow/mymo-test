9805c4d851a548b8a6c68d6a80b8ef10892e13d6
Omer Katz <omer.drow@gmail.com>
2018-01-01 13:48:01 +0200

    Fix default postgres URI.

--- reddit_scrapper/settings.py
+++ reddit_scrapper/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'reddit_scrapper.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://')
+    'default': dj_database_url.config(default='postgres://postgres:postgres@localhost/postgres')
 }
 
 
