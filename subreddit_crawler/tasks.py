cfd8565d7b118ac30fc1c06a6348fb9660747de9
Omer Katz <omer.drow@gmail.com>
2018-01-02 15:22:52 +0200

    Test the codepath where the subreddit was already crawled.

--- subreddit_crawler/tasks.py
+++ subreddit_crawler/tasks.py
@@ -1,8 +1,11 @@
+from datetime import date, datetime
+import time
+
 import praw
 from django.conf import settings
 
 from subreddit_crawler.celery import app
-from subreddit_crawler.models import SubredditSubmission
+from subreddit_crawler.models import Subreddit, SubredditSubmission
 
 
 def create_reddit_client():
@@ -25,3 +28,21 @@ def scrape_subreddit(subreddit_name, start_date, end_date=None):
     ]
     if submissions:
         SubredditSubmission.objects.bulk_create(submissions)
+
+
+@app.task
+def crawl_subreddits():
+    subreddits = Subreddit.objects.all()
+
+    for subreddit in subreddits:
+        if subreddit.last_crawled is None:
+            # Start crawling from today
+            timestamp = time.mktime(datetime.combine(date.today(),
+                                    datetime.min.time()).timetuple())
+        else:
+            timestamp = time.mktime(subreddit.last_crawled.timetuple())
+
+        scrape_subreddit.delay(subreddit.name, start_date=timestamp)
+
+        subreddit.last_crawled = datetime.now()
+        subreddit.save(update_fields=['last_crawled'])
--- tests/unit/subreddit_crawler/test_tasks.py
+++ tests/unit/subreddit_crawler/test_tasks.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+import time
 from unittest import mock
 
 import pytest
 from django.conf import settings
 
-from subreddit_crawler.tasks import create_reddit_client, scrape_subreddit
+from subreddit_crawler.tasks import (crawl_subreddits, create_reddit_client,
+                                     scrape_subreddit)
 
 
 @pytest.fixture
@@ -13,7 +16,19 @@ def mocked_reddit_client(mocker):
 
 @pytest.fixture
 def mocked_subreddit_submission(mocker):
-    return mocker.patch('subreddit_crawler.tasks.SubredditSubmission', autospec=True)
+    return mocker.patch('subreddit_crawler.tasks.SubredditSubmission',
+                        autospec=True)
+
+
+@pytest.fixture
+def mocked_subreddit(mocker):
+    return mocker.patch('subreddit_crawler.tasks.Subreddit',
+                        autospec=True)
+
+
+@pytest.fixture
+def mocked_scrape_subreddit(mocker):
+    return mocker.patch('subreddit_crawler.tasks.scrape_subreddit')
 
 
 @pytest.fixture(scope='session')
@@ -22,8 +37,18 @@ def subreddit(faker):
 
 
 @pytest.fixture(scope='session')
+def subreddit2(faker):
+    return faker.slug()
+
+
+@pytest.fixture(scope='session')
 def start_date(faker):
     return faker.unix_time()
+
+
+@pytest.fixture
+def current_date():
+    return datetime.now().date()
 
 
 def test_create_reddit_client(mocked_reddit_client):
@@ -69,3 +94,34 @@ def test_scrape_subreddit_insert_new_submissions(mocked_reddit_client,
         mocked_subreddit_submission(
             id=submission2.id, text=submission2.selftext)
     ])
+
+
+@pytest.mark.freeze_time('2018-01-01 12:00:00')
+def test_crawl_subreddits(mocked_subreddit,
+                          mocked_scrape_subreddit,
+                          subreddit,
+                          subreddit2,
+                          current_date):
+    mocked_subreddit1 = mock.Mock()
+    mocked_subreddit1.name = subreddit
+    mocked_subreddit1.last_crawled = None
+
+    mocked_subreddit2 = mock.Mock()
+    mocked_subreddit2.name = subreddit2
+    mocked_subreddit2.last_crawled = datetime.now()
+
+    mocked_subreddit.objects.all.return_value = [
+        mocked_subreddit1,
+        mocked_subreddit2
+    ]
+
+    crawl_subreddits()
+
+    start_date1 = time.mktime(current_date.timetuple())
+    start_date2 = time.mktime(datetime.now().timetuple())
+
+    mocked_scrape_subreddit.delay.assert_has_calls([
+            mock.call(subreddit, start_date=start_date1),
+            mock.call(subreddit2, start_date=start_date2)
+        ]
+    )
